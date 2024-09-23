aa5ca3e081a4c9e3c7de567588eb68ae75f7f6fe
bach.ngongoc <nnbach1997@gmail.com>
2024-09-23 23:04:57 +0700

    Uodate code Collection, Instantiate

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ var Blog = Backbone.Model.extend({
     url: "",
   },
 });
+
+// Bacnkbone Collection
+var Blogs = Backbone.Collection.extend({});
+
+// Instantiate two Blogs
+var blog1 = new Blog({
+  author: "Bach",
+  title: "Bach Dzai",
+  url: "bach.com",
+});
+
+var blog2 = new Blog({
+  author: "Trang",
+  title: "Trang Zinh gai",
+  url: "trang.com",
+});
+
+var blogs = new Blogs([blog1, blog2]);
